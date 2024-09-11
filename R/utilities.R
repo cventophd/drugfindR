ceaa16fdbce189139900dbd84351ac22ff182fde
cventophd <93550258+cventophd@users.noreply.github.com>
2024-09-11 12:05:46 -0400

    Update utilities.R
    Signed-off-by: cventophd <93550258+cventophd@users.noreply.github.com>

--- R/utilities.R
+++ R/utilities.R
@@ -25,8 +25,9 @@ targetRename <- function(inputNames) {
 #' Check if the library is valid
 #'
 #' @param lib a string of libraries
-#'
+#' Note: This lib of strings comes from the validateLibraries function. 
 #' @return a boolean
+# The output of this string for each indiviudal input is a 0 for false and 1 for true. This is only to ensure the library itself exists.
 .validateLibrary <- function(lib) {
     lib %in% c("CP", "KD", "OE")
 }
@@ -34,8 +35,11 @@ targetRename <- function(inputNames) {
 #' Check if the libraries input are valid
 #'
 #' @param libs a character vector of libraries
-#'
+#' #the character vectors must be "OE", "KD", or "CP". The purpose of this function is to ensure the that the input is one of these.
 #' @return a boolean
+# this return a 0 or a 1 to ensure if the result is true or false
+# this function will go through each indiviudal element in libs using purrr:map_lgl and will run the individual .validateLibrary function to ensure all entires are
+# of the stirng "CP", "KD", or "OE".
 validateLibraries <- function(libs) {
     all(purrr::map_lgl(libs, .validateLibrary))
 }
@@ -43,8 +47,10 @@ validateLibraries <- function(libs) {
 #' Stop if the libraries are invalid
 #'
 #' @param libs a character vector of libraries
-#'
+#' this function will itterate through all the strings in libs and look to see if 'OE', 'KD', 'CP' is contained in all entries
 #' @return a stop if the libraries are invalid
+# if not all the entires is not 'OE', 'KD', and 'CP' - then the function 
+#this function will run is if validatelbiariers stop and will indicate the one of the strings is not 'OE', 'KD', and 'CP'.
 stopIfInvalidLibraries <- function(libs) {
     if (!validateLibraries(libs)) {
         stop("Both input and output libraries must be one of 'OE', 'KD', 'CP'")
@@ -54,8 +60,9 @@ stopIfInvalidLibraries <- function(libs) {
 #' Load the correct metadata table
 #'
 #' @param lib a string. One of "OE", "KD" or "CP"
-#'
+#'this takes either 'OE', 'KD', or 'CP' string and will create metadata table.
 #' @return a tibble
+# the tibble will be the meta-data table associated with the 'OE', 'KD' or 'CP'. If the string is not one of these - the string will return 'Invalid library'.
 loadMetadata <- function(lib) {
     if (lib == "OE") {
         oeMetadata
